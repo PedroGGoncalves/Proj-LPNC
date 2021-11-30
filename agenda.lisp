2b3b9705c6df2de28506b1405b4471f9cf1d9d13
Pedro Gabriel Gonçalves <85896745+PedroGGoncalves@users.noreply.github.com>
2021-11-30 13:14:38 -0300

    Update agenda.lisp

--- agenda.lisp
+++ agenda.lisp
@@ -1,6 +1,13 @@
-(defun data (hora dia mes ano)
-(if (and (= hora 0)( hora 24)( dia 0)(= dia 30)( mes 0)(= mes 12))
-(print Data valida)
-(print Nao valido)
+(defun data (hora dia mes ano) ;;data -> ainda vamos melhorar
+(if (and (>= hora 0)(< hora 24)(> dia 0)(<= dia 30)(> mes 0)(<= mes 12))
+(print "Data valida") ;; mudar o print - será um retorno
+(print "Nao valido") ;; será retorno tbm
+)
+)
+(defun evento (horai diai mesi anoi &optional horaf diaf mesf anof);;o fim fica optativo
+(data horai diai mesi anoi)
+(if(eql horaf nil) ;;omissao fim,assume se uma hora de evento
+(data (+ horai 1) diai mesi anoi)
+(data horaf diaf mesf anof) ;; se nao houve omissao
 )
 )
